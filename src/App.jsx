ae7d24c56ab8d02293fc4a7a9d480c88cc1c5b73
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-04-08 16:18:54 +0200

    feat: create dynamic route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Components/NavBar/index.jsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home/index.jsx";
 import CreatePost from "./pages/CreatePost/index.jsx";
+import Post from "./pages/Post/index.jsx";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route path={"/"} element={<Home />} />
                 <Route path={"/add-post"} element={<CreatePost />} />
+                <Route path={"/post/:postId"} element={<Post />}/>
             </Routes>
         </>
     )
